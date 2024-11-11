c66baccea9a93479545747ffe877bb08d6a9bf84
mfridsen <120990597+mfridsen@users.noreply.github.com>
2024-11-11 15:24:06 +0100

    SpringBoot up n running

---
+++ src/main/java/dev/groupeighteen/Application.java
@@ -0,0 +1,18 @@
+package dev.groupeighteen;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mattias Fridsén
+ * @project Assignment3EA
+ * @package dev.groupeighteen
+ * @contact matfir-1@student.ltu.se
+ * @date 2024-11-08 14:48:11
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
