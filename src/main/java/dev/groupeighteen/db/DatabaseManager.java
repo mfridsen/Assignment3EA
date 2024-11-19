17ab35e8e483ad5995b9de9343dd8988bc48e9ee
mfridsen <120990597+mfridsen@users.noreply.github.com>
2024-11-19 14:32:33 +0100

    more basic structure

---
+++ src/main/java/dev/groupeighteen/db/DatabaseManager.java
@@ -0,0 +1,13 @@
+package dev.groupeighteen.db;
+
+/**
+ * @author Mattias Fridsén
+ * @project Assignment3EA
+ * @package dev.groupeighteen.db
+ * @contact matfir-1@student.ltu.se
+ * @date 2024-11-19 14:32:18
+ */
+public class DatabaseManager
+{
+
+}
---
+++ src/main/java/dev/groupeighteen/gui/GUI.java
@@ -0,0 +1,13 @@
+package dev.groupeighteen.gui;
+
+/**
+ * @author Mattias Fridsén
+ * @project Assignment3EA
+ * @package dev.groupeighteen.gui
+ * @contact matfir-1@student.ltu.se
+ * @date 2024-11-19 14:31:18
+ */
+public class GUI
+{
+
+}
