7a04bc1c7aeeda81439731ee30eaf6f70a600afa
Nils Fritzner <nilsfritzner@hotmail.com>
2024-11-26 13:51:50 +0100

    Something something javaFX scene builder

--- src/main/java/dev/groupeighteen/gui/GUI.java
+++ src/main/java/dev/groupeighteen/gui/GUI.java
@@ -1,13 +1,52 @@
 package dev.groupeighteen.gui;
 
-/**
- * @author Mattias Fridsén
- * @project Assignment3EA
- * @package dev.groupeighteen.gui
- * @contact matfir-1@student.ltu.se
- * @date 2024-11-19 14:31:18
- */
-public class GUI
-{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 
+
+public class GUI extends Application {
+    private ConfigurableApplicationContext applicationContext;
+
+
+    @Override
+    public void init() throws Exception {
+        applicationContext = new SpringApplicationBuilder(SpringBootApplication.class).run();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlloader = new FXMLLoader(getClass().getResource("StudentGrades.fxml"));
+        Parent root = fxmlloader.load();
+
+
+        Scene scene = new Scene(root);
+
+
+        applicationContext.publishEvent(new StageReadyEvent(stage));
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        applicationContext.close();
+        Platform.exit();
+    }
+
+    static class StageReadyEvent extends ApplicationEvent {
+        public StageReadyEvent(Stage stage) {
+            super(stage);
+        }
+
+        public Stage getStage() {
+            return ((Stage) getSource());
+        }
+    }
 }
