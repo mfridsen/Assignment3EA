ee21ccc6f9f11331c31fbbd14f48af8b6f2e5607
Nils Fritzner <nilsfritzner@hotmail.com>
2024-11-28 14:16:23 +0100

    FXML layout done

--- src/main/java/dev/groupeighteen/gui/StudentGrades.java
+++ src/main/java/dev/groupeighteen/gui/StudentGrades.java
@@ -1,12 +1,20 @@
 package dev.groupeighteen.gui;
-
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 
 import javafx.fxml.FXML;
 
 public class StudentGrades {
 
-    @FXML
-    private void initialize() {
 
+    @FXML
+    private MenuButton saveDraftButton;
+
+
+    @FXML
+    public void initialize() {
+        for (MenuItem item : saveDraftButton.getItems()) {
+            item.setOnAction(event -> saveDraftButton.setText(item.getText()));
+        }
     }
 }
